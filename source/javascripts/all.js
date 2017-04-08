c124a6cb29577d4cd214545f32c1d74caacb8e12
Guayo Mena <guayo.mena@gmail.com>
2017-04-08 17:32:49 -0600

    Click tracing a los botones de comprar entradas

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,10 @@
+//Google analytics click tracking
+function clickComprarEntradas(concierto) {
+  ga('send', 'event', 'Clicks', 'Comprar Entradas', concierto.getAttribute("data-concierto"));
+}
+
 $(document).ready(function(){
+  //Botón de ver detalles
   $('.ver-detalles-button').unbind('click').click(function(){
     $(".detalles").toggleClass("open");
     $(this).toggleClass("ocultar");
