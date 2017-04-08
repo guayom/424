e95e2d8b43a374bd83fdbdbea15b7fc50fd1fbe8
Guayo Mena <guayo.mena@gmail.com>
2017-04-08 14:36:04 -0600

    Arreglé el botón de más detalles

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,6 @@
-// This is where it all goes :)
+$(document).ready(function(){
+  $('.ver-detalles-button').unbind('click').click(function(){
+    $(".detalles").toggleClass("open");
+    $(this).toggleClass("ocultar");
+  });
+});
