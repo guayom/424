1894f084d2dc39d521880f6533d2235279cb174d
guayom <guayo.mena@gmail.com>
2018-09-08 00:11:21 -0600

    Add spotify player

--- src/components/spotify.js
+++ src/components/spotify.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import SpotifyPlayer from 'react-spotify-player';
+
+// size may also be a plain string using the presets 'large' or 'compact'
+const size = {
+  width: '100%',
+  height: 300,
+};
+const view = 'coverart'; // or 'coverart'
+const theme = 'black'; // or 'white'
 
 export default () => 
   <div>
-    SPOTIFY
+    <SpotifyPlayer
+      uri="spotify:album:5fbgtt8EgFk7Lthbc5UWAh"
+      size={size}
+      view={view}
+      theme={theme}
+    />
   </div>
