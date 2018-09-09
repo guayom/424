e0767f970b556e953d4fbe340dddfa60e2d3a37c
guayom <guayo.mena@gmail.com>
2018-09-09 17:33:26 -0600

    Make track list layout

--- src/pages/letras.js
+++ src/pages/letras.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { ProductsGrid } from '../components/grids'
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
 import Link from 'gatsby-link';
 import letra from '../templates/letra';
 import {MainWrapper} from '../components/wrappers';
@@ -11,6 +12,37 @@ const ImageContainer = styled.div`
   margin-bottom: ${props => props.theme.spacing.base / 2}px;
 `
 
+const Album = styled.div`
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  grid-gap: ${props => props.theme.spacing.base }px;
+  margin-bottom: ${props => props.theme.spacing.base }px;
+
+  ${breakpoint('tablet')`
+    grid-template-columns: 300px 1fr;
+  `}
+`
+
+const Title = styled.h3`
+  margin: 0;
+  font-weight: bold;
+  margin-bottom: ${props => props.theme.spacing.base / 2}px;
+`
+
+const TrackList = styled.ol`
+  list-style-type: decimal;
+  margin: 0;
+  padding: 0;
+  padding-left: 20px;
+
+  li {
+    padding-left: 5px;
+  }
+  a {
+    color: #000;
+  }
+`
+
 const Tienda = ({ data }) => {
   const albums = data.albums.edges.map(p => p.node).filter(a => a.letras != null);
   const letras = data.letras.edges.map(p => p.node);
@@ -18,21 +50,23 @@ const Tienda = ({ data }) => {
   return (
     <MainWrapper>
       {albums.map(album => 
-        <div key={album.id}>
+        <Album key={album.id}>
           <ImageContainer>
             <Img sizes={album.cover.sizes} alt={album.titulo} />
           </ImageContainer>
-          <h3>{album.titulo}</h3>
-          <ol>
-            {letras.filter(l => l.release.id === album.id).sort((a,b) => (a.trackNumber - b.trackNumber)).map( track => 
-              <li key={track.id}>
-                <Link to={`/letras/${slugify(album.titulo, {lower: true})}/${slugify(track.tema, {lower: true})}`}>
-                  {track.trackNumber} - {track.tema}
-                </Link>
-              </li>
-            )}
-          </ol>
-        </div>
+          <div>
+            <Title>{album.titulo}</Title>
+            <TrackList>
+              {letras.filter(l => l.release.id === album.id).sort((a, b) => (a.trackNumber - b.trackNumber)).map(track =>
+                <li key={track.id}>
+                  <Link to={`/letras/${slugify(album.titulo, { lower: true })}/${slugify(track.tema, { lower: true })}`}>
+                    {track.tema}
+                  </Link>
+                </li>
+              )}
+            </TrackList>
+          </div>
+        </Album>
       )}
     </MainWrapper>
   )
